e07cb3492e9af7d8f39d0cad43b4a4db9eb7e31d
PriyathamVarma <priyatham002@gmail.com>
2024-02-20 17:42:25 +0000

    Feature test: auto0 login

--- src/app/components/templates/header.tsx
+++ src/app/components/templates/header.tsx
@@ -1,18 +1,30 @@
 "use client";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 const Header = () => {
+  const { user, error, isLoading } = useUser();
+  console.log("user \n", user);
+  console.log("Error \n", error);
+  console.log("Loading \n", isLoading);
+
   return (
     <header className="bg-white shadow py-2 w-full h-26">
       <div className="container mx-auto flex items-center justify-around">
         {/* Logo and company name */}
         <div className="flex items-center">
-          <Link href="/">Trace X</Link>
+          <Link href="/">Verdascope</Link>
         </div>
 
         {/* Navigation links */}
-        <nav className="hidden lg:flex items-center space-x-8 text-gray-500"></nav>
+        <nav className="hidden lg:flex items-center space-x-8 text-gray-500">
+          {user != undefined ? (
+            <Link href="/api/auth/logout">Logout {user.name}</Link>
+          ) : (
+            <Link href="/api/auth/login">Login</Link>
+          )}
+        </nav>
 
         {/* Mobile menu button */}
         <button className="lg:hidden focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 p-2">
