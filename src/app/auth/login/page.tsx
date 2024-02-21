9c5a81aff89ced47e6da23bebc21db2ce3057e40
PriyathamVarma <priyatham002@gmail.com>
2024-02-21 12:32:55 +0000

    feat:login page

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Link from "next/link";
+import LoginForm from "@/app/components/organisms/forms/auth/login";
+import Image from "next/image";
+import imgSrc from "../../../../public/images/main.png";
+
+const LoginPage = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="border border-black w-full min-h-screen flex items-center justify-center bg-black">
+        <LoginForm />
+      </div>
+      <div className="w-full h-full flex items-center justify-center overflow-hidden">
+        <Image src={imgSrc} objectFit="cover" alt="Picture of the author" />
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
